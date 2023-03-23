3dacd8e8b6365705f2c47b8af3cbda76c8bd01eb
IssaZeidan <Issa.Zeidan94@gmail.com>
2023-03-24 01:18:42 +0300

    added index.html and style.css also app.js with a higherOrderFunction.js to the repo

---
+++ higherOrderFunction.js
@@ -0,0 +1,48 @@
+// Q number 5
+
+// let x = [1, 2, 3];
+
+// for (let i = 0; i < x.length; i++) {
+//   x[i] = 2 ** x[i];
+// }
+// console.log(x);
+
+// let arrayForEach = [1, 2, 3];
+// arrayForEach.forEach((value, index, arrayForEach) => {
+//   arrayForEach[index] = 2 ** value;
+// });
+// console.log(arrayForEach);
+
+// let xArr = x.map((value) => {
+//   return 2 ** value;
+// });
+// console.log(xArr);
+
+// Q number 6
+
+// Arr=[1,2,3,"Issa"];
+// let ArrMap= Arr.map ((value)=>{
+// if (ArrNaN= ("value")){return "N/A" }
+// else if ("value%2==0"){return "even"}
+// else {return "odd";}
+// });
+// console.log(Arr);
+
+// Q number 7
+
+// let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// function fizzbuzz(x) {
+//   let xMap = x.map((value) => {
+//     if (value % 3 == 0 && value % 5 == 0) {
+//       return "FizzBuzz";
+//     } else if (value % 3 == 0) {
+//       return "Fizz";
+//     } else if (value % 5 == 0) {
+//       return "Buzz";
+//     } else {
+//       return value;
+//     }
+//   });
+//   return xMap;
+// }
+// console.log(fizzbuzz(x));
